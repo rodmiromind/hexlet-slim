fc99bdbf20c697c5cf81e384ce89ad16a6f7a909
notebook <onetreearmy@gmail.com>
2019-09-15 20:01:43 +0300

    get/post

--- public/index.php
+++ public/index.php
@@ -9,4 +9,12 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
+
+$app->get('/users', function ($request, $response) {
+    return $response->write('GET /users');
+});
+
+$app->post('/users', function ($request, $response) {
+    return $response->write('POST /users');
+});
 $app->run();
