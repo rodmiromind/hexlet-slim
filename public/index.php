33a44745987735606d44a938e74baf5b02e2597c
notebook <onetreearmy@gmail.com>
2019-09-29 20:35:39 +0300

    as

--- public/index.php
+++ public/index.php
@@ -2,7 +2,14 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
@@ -10,11 +17,41 @@ $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
 
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
-$app->post('/users', function ($request, $response) {
-    return $response->write('POST /users');
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
 });
+
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => [],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+});
+
+$app->post('/users', function ($request, $response){
+    $id = rand(1, 100);
+    $user = $request->getParsedBodyParam('user');
+    var_dump($user);
+    $errors = $validator->validate($user);
+    if (count($errors) === 0) {
+        $repo->save($user);
+        return $response->withHeader('Location', '/')
+          ->withStatus(302);
+    }
+    $params = [
+        'user' => $user,
+        'errors' => $errors
+    ];
+    return $this->get('renderer')->render($response, "users/new.phtml", $params);
+});
+
 $app->run();
